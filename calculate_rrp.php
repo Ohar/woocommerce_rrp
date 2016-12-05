ceae1cc1763918c43a3aea93a1be203b4bcd6591
Pavel lysenko <p.lysenko@zmeke.com>
2016-12-05 17:58:40 +0300

    1.0.3 Calculate back percents

--- calculate_rrp.php
+++ calculate_rrp.php
@@ -5,19 +5,18 @@ defined( 'ABSPATH' ) or die( '' );
 function calculate_rrp ($buy_price, $params) {
 	$costs = $buy_price + $params['ads_cost'] + $params['shipping_cost'] + $params['package_cost'];
 	
-	$costs_min_profit = $costs + $params['min_profit'];
-	$costs_desired_profit = $costs + calc_percents($costs, $params['desired_profit']);
-	$costs_to_get_max = array($costs_min_profit, $costs_desired_profit);
+	$costs_min_profit = $costs + $params['min_profit'];	
+	$costs_desired_profit = add_back_percents($costs, $params['desired_profit']);
 	
-	$costs_with_profit = max($costs_to_get_max);
-	$tax = calc_percents($costs_with_profit, $params['tax_rate']);
+	$costs_with_profit = max(array($costs_min_profit, $costs_desired_profit));	
 	
-	$rrp = $costs_with_profit + $tax;
+	$rrp = add_back_percents($costs_with_profit, $params['tax_rate']);	
+	
 	$rrp_ceiled = ceil($rrp / 10) * 10;
 	
 	return $rrp_ceiled;
 }
 
-function calc_percents ($val, $percents) {	
-	return round($val * $percents / 100);
+function add_back_percents ($val, $percents) {	
+	return round($val / (1 - $percents / 100));
 }
